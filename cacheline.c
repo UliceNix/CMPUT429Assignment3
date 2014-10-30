c7ca29220067c3faef175749e697c58f2ad93c4c
Alice Wu <mingxun@ualberta.ca>
2014-10-30 00:11:10 -0600

    Update cacheline.c

--- cacheline.c
+++ cacheline.c
@@ -10,16 +10,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <math.h>
 
 #define KB 1024
+#define MB 1024 * KB
 #define CACHE_SIZE 128 * KB
-#define ARRAY_SIZE 130 * KB
+#define ARRAY_SIZE 3 * MB
 #define ARRAY_LENGTH ARRAY_SIZE/sizeof(int)
 #define MAX_LINE_SIZE 512
-#define RUNS 4
+#define RUNS 10
 
+long long wall_clock_time();
 
 void init_array(int *start){
         int i;       
@@ -29,7 +31,7 @@ void init_array(int *start){
         }
 }
 
-void init_time(double *start){
+void init_time(float *start){
         int i;
         for(i = 0; i < ARRAY_LENGTH; i++){
             start[i] = 0.00;
@@ -41,15 +43,15 @@ int main()
         int line_size, run, sum;
 
         /* used to record start time and end time*/
-        struct timeval start, end;
-        double t1, t2, time_accu;
+        long long start, end;
+        float t1, t2, time_accu;
         
         /* Allocate an integer array and initialize it*/
         int* array = malloc(ARRAY_SIZE);
         int lengthmod = ARRAY_LENGTH - 1;
 
         /* Allocate a double array to record time */
-        double* time_array = malloc(ARRAY_LENGTH*sizeof(double));
+        float* time_array = malloc(ARRAY_LENGTH*sizeof(double));
 
         FILE *fp;
 
@@ -61,36 +63,62 @@ int main()
             perror("fopen error!\n"); exit(10);
         }
 
-        for(line_size = 4; line_size < MAX_LINE_SIZE; line_size*=2){
+        for(line_size = 4; line_size < MAX_LINE_SIZE/sizeof(int); line_size*=2){
             time_accu = 0;
             for(run = 0; run < RUNS; run++){
 
                 /* start timer*/
-                gettimeofday(&start, NULL);
-                t1 = start.tv_usec + start.tv_sec * 1000000;
-
+                start = wall_clock_time();
                 int k =0;
                 for(k; k < 512 * 1024;k++){
-                    //printf("line_size:%d, k:%d, /k*i&lengthmod:%d\n",line_size, k, (k*line_size)&lengthmod);
-                    // should do a sum to prevent compiler from optimizing
-                    sum += array[(k*line_size)&lengthmod];
+                    sum += array[(k*line_size) & lengthmod];
                     
                 }
-                gettimeofday(&end, NULL);
-                t2 = end.tv_usec + end.tv_sec * 1000000;
-
-                /* record the time of run */
-                time_accu += t2 - t1;
+                end = wall_clock_time();
+                time_array[run] = ((float)(end-start))/1000000000;
+                if( fprintf(fp, "line_size: %d, Sum: %d, Time: %lf\n", line_size, sum, time_array[run]) < 0){
+                    perror("fprint error\n"); exit(10);
+                }
             }
-            time_accu = time_accu/RUNS;
+            
+            int c,d;
+            for(c = 0; c < RUNS-1; c++){
+                for(d = 0; d < RUNS-c-1; d++){
+                    if(time_array[d] > time_array[d+1]){
+                        float tv;
+                        tv = time_array[d];
+                        time_array[d] = time_array[d+1];
+                        time_array[d+1] = tv;
+                    }
+                }
+            }
+            for(c = 0; c < RUNS; c++){
+                printf("%lf \n", time_array[c]);
+            }
+
+            printf("size:%3lu Time:%lf\n", line_size*sizeof(int), time_array[RUNS/2]);
+/*
             if( fprintf(fp, "line_size: %d, Sum: %d, Time: %lf\n", line_size, sum, time_accu) < 0){
                 perror("fprint erroe\n"); exit(10);
             }
-            
+            */
+            FILE* temp = fopen("/dev/null","w");
+            fprintf(temp, "%d", sum);
         }
 
         if(fclose(fp)){
             perror("encountered fclose error"); exit(10);
         }
-
+        
+        free(array);
+        free(time_array);
 }
+
+
+long long wall_clock_time() {
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        return (long long)(tv.tv_usec * 1000 + (long long)tv.tv_sec * 1000000000ll);
+}
+
+
